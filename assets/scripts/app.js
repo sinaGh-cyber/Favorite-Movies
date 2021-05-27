2e05024878fa64a11b26288886463d48a07e0900
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-27 19:25:18 +0430

    cancelButton event lestener added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,17 @@
 const addMovieModal = document.getElementById(`add-modal`);
 const StartAddMovieButton = document.querySelector(`header button`);
 const backgroundDarker = document.getElementById(`backdrop`);
+const cancelButton = document.querySelector(`.modal__actions button`);
 const backdroptoggler = () => {backgroundDarker.classList.toggle(`visible`);};
-const StartAddMovieButtonToggler = () => {addMovieModal.classList.toggle(`visible`);};
+const StartAddMovieButtonToggler = () => {
+    addMovieModal.classList.toggle(`visible`);
+    backdroptoggler();
+};
+const backdropClickHandeler = () => {
+    StartAddMovieButtonToggler();
+}; 
 //Add Movie button functionality:
 StartAddMovieButton.addEventListener("click" , StartAddMovieButtonToggler );
-StartAddMovieButton.addEventListener(`click`, backdroptoggler);
 //backDrope cancleing:
-backgroundDarker.addEventListener(`click`, backdroptoggler);
-backgroundDarker.addEventListener(`click`, StartAddMovieButtonToggler);
-
-
+backgroundDarker.addEventListener(`click`, backdropClickHandeler);
+cancelButton.addEventListener(`click`, backdropClickHandeler);
