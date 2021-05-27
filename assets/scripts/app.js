9446c33bb76f7a247c6e728c5c90123db58fb896
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-27 17:27:42 +0430

    backgroundDarker ebent listener added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const addMovieModal = document.getElementById(`add-modal`);
-const addButton = document.querySelector(`header button`);
-console.log(addButton);
-addButton.addEventListener("click" , () => {addMovieModal.classList.toggle(`visible`);} );
+const StartAddMovieButton = document.querySelector(`header button`);
+const backgroundDarker = document.getElementById(`backdrop`);
+StartAddMovieButton.addEventListener("click" , () => {addMovieModal.classList.toggle(`visible`);} );
+StartAddMovieButton.addEventListener(`click`, () => {backgroundDarker.classList.toggle(`visible`);});
 
