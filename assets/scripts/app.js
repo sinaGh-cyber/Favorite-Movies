ce223b61e4d08a0e9653206d0ba8729a4dfc8c15
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-27 18:44:23 +0430

    backgroundDarker cacleing event litener added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,11 @@
 const addMovieModal = document.getElementById(`add-modal`);
 const StartAddMovieButton = document.querySelector(`header button`);
 const backgroundDarker = document.getElementById(`backdrop`);
-StartAddMovieButton.addEventListener("click" , () => {addMovieModal.classList.toggle(`visible`);} );
-StartAddMovieButton.addEventListener(`click`, () => {backgroundDarker.classList.toggle(`visible`);});
+const backdroptoggler = () => {backgroundDarker.classList.toggle(`visible`);};
+const StartAddMovieButtonToggler = () => {addMovieModal.classList.toggle(`visible`);};
+StartAddMovieButton.addEventListener("click" , StartAddMovieButtonToggler );
+StartAddMovieButton.addEventListener(`click`, backdroptoggler);
+backgroundDarker.addEventListener(`click`, backdroptoggler);
+backgroundDarker.addEventListener(`click`, StartAddMovieButtonToggler);
+
 
