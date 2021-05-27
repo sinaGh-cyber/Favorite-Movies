64f22329f96fa15b630cdb33110b1a10da962b4e
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-27 18:47:02 +0430

    adding some comments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,10 @@ const StartAddMovieButton = document.querySelector(`header button`);
 const backgroundDarker = document.getElementById(`backdrop`);
 const backdroptoggler = () => {backgroundDarker.classList.toggle(`visible`);};
 const StartAddMovieButtonToggler = () => {addMovieModal.classList.toggle(`visible`);};
+//Add Movie button functionality:
 StartAddMovieButton.addEventListener("click" , StartAddMovieButtonToggler );
 StartAddMovieButton.addEventListener(`click`, backdroptoggler);
+//backDrope cancleing:
 backgroundDarker.addEventListener(`click`, backdroptoggler);
 backgroundDarker.addEventListener(`click`, StartAddMovieButtonToggler);
 
